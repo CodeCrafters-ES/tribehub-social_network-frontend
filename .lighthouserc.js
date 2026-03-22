50fdbb4d742255b6653cad01c30c8251235099af
Thalia <xaxu2009.tgs@gmail.com>
2026-03-22 21:48:31 +0100

    fix: paso lighthouse con autorun

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,20 +1,21 @@
 module.exports = {
   ci: {
     collect: {
+      // Como no hay servidor externo, LHCI levantará el tuyo
       startServerCommand: 'npm run start',
       url: ['http://localhost:3000'],
-      numberOfRuns: 3,
+      numberOfRuns: 1,
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', {minScore: 0.9}],
-        'categories:accessibility': ['error', {minScore: 0.9}],
-        'categories:best-practices': ['error', {minScore: 0.9}],
-        'categories:seo': ['error', {minScore: 0.9}],
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
       },
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'temporary-public-storage', // Sube el reporte a los servidores de Google
     },
   },
 };
